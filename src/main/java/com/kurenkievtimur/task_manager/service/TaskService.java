b763c46b12785b9532c4623cd990c3eadcc56de2
KurenkievTimur <ti-mur99@mail.ru>
2022-03-06 16:07:19 +0600

    Добавил 2 метода save, findAll

--- src/main/java/com/kurenkievtimur/task_manager/service/TaskService.java
+++ src/main/java/com/kurenkievtimur/task_manager/service/TaskService.java
@@ -1,4 +1,11 @@
 package com.kurenkievtimur.task_manager.service;
 
+import com.kurenkievtimur.task_manager.DTO.TaskDTO;
+import com.kurenkievtimur.task_manager.entity.Task;
+
+import java.util.List;
+
 public interface TaskService {
+    void save(TaskDTO taskDTO);
+    List<Task> findAll();
 }
